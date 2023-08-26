4fd0bf0f747e9516201586b3a286d1c5d8e476f0
andrebettencourt7 <andreluispb10@gmail.com>
2023-08-25 21:50:28 -0300

    conectando com banco de dados

---
+++ Ex-json/script.js
@@ -0,0 +1,43 @@
+const http = require('http');
+
+const fs = require('fs')
+
+//importar mysql:
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@12345678",
+    database: "escola"
+});
+
+//Criar um Database = schema
+
+
+
+function readFile(response, file) {
+    fs.readFile(file, function (err, data) {
+        response.end(data)
+    })
+}
+
+const server = http.createServer((request, response) => {
+    if (request.url === '/alunos') {
+        //Pega as informações no banco de dados e expoe:
+        con.query("SELECT * FROM alunos", function (err, result, fields) {
+            if (err) throw err;
+            response.end(JSON.stringify(result))
+            console.log(result);
+        });
+    } else {
+        response.end(JSON.stringify({
+            erro: true,
+            message: "Data not found...."
+        }))
+    }
+})
+
+server.listen(3001, () => {
+    console.log("servidor no ar")
+})
