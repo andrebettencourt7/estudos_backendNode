1e0aa1ac30decdde90e628690b7f196b3646d473
andrebettencourt7 <andreluispb10@gmail.com>
2023-09-01 19:25:45 -0300

    Criando endpoin CRUD

--- Ex-json/script.js
+++ Ex-json/script.js
@@ -1,4 +1,4 @@
-const http = require ('http');
+const http = require('http');
 
 const fs = require('fs')
 
@@ -15,7 +15,6 @@ var con = mysql.createConnection({
 //Criar um Database = schema
 
 
-
 function readFile(response, file) {
     fs.readFile(file, function (err, data) {
         response.end(data)
@@ -23,20 +22,58 @@ function readFile(response, file) {
 }
 
 const server = http.createServer((request, response) => {
-    if (request.url === '/alunos') {
+    if (request.url === '/inserir') {
+        con.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+            var sql = "INSERT INTO alunos (nome, telefone, email, escola) VALUES ('Joaquin', '1199998888', 'joaquin@email.com', 'SENAI')";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+            });
+
+            response.end(JSON.stringify({message: "Salvos"}))
+        });
+
+    } else if (request.url === '/visualizar') {
         //Pega as informações no banco de dados e expoe:
         con.query("SELECT * FROM alunos", function (err, result, fields) {
             if (err) throw err;
             response.end(JSON.stringify(result))
             console.log(result);
         });
-    } else {
+
+    } else if (request.url === '/deletar') {
+        con.connect(function (err) {
+            if (err) throw err;
+            var sql = "DELETE FROM alunos WHERE telefone = '1199998888'";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("Number of records deleted: " + result.affectedRows);
+            });
+
+            response.end(JSON.stringify({message: "Deletado"}))
+        });
+
+    } else if (request.url === '/update') {
+        con.connect(function (err) {
+            if (err) throw err;
+            var sql = "UPDATE alunos SET email = 'joaquin@email.com' WHERE email = 'joaquin2@email.com'";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result.affectedRows + " record(s) updated");
+            });
+            response.end(JSON.stringify({message: "Update"}))
+        });
+
+    }else {
         response.end(JSON.stringify({
             erro: true,
             message: "Data not found...."
         }))
     }
 })
+
 
 server.listen(3001, () => {
     console.log("servidor no ar")
